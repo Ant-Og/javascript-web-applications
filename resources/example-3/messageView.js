2ddbe6e839657b3ed92e4ce3d7701d0a008edf36
Anthony O <hackerman.ant@gmail.com>
2023-11-08 12:53:09 +0000

    hideMessage method complete

--- resources/example-3/messageView.js
+++ resources/example-3/messageView.js
@@ -1,9 +1,14 @@
 class MessageView {
   constructor() {
     this.buttonEl = document.querySelector('#show-message-button');
+    this.hideButtonEl = document.querySelector('#hide-message-button');
 
     this.buttonEl.addEventListener('click', () => {
        this.displayMessage();
+    });
+
+    this.hideButtonEl.addEventListener('click', () => {
+      this.hideMessage();
     });
   }
 
@@ -14,6 +19,10 @@ class MessageView {
     document.querySelector('#main-container').append(messageDivEl);
     console.log('Thanks for clicking me!');
   }
-}
 
+  hideMessage() {
+    const elementToRemove = document.querySelector('#message');
+    elementToRemove.remove(); 
+  }
+}
 module.exports = MessageView;
