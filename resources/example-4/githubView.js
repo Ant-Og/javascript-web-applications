18ebacd0978db076f956dfa020846d805ec6e704
Anthony O <hackerman.ant@gmail.com>
2023-11-14 17:38:14 +0000

    display method for fetched repo info complete

--- resources/example-4/githubView.js
+++ resources/example-4/githubView.js
@@ -11,12 +11,23 @@ class GithubView {
 
       this.client.getRepoInfo(repoName, repoData => {
         console.log(repoData);
+        this.repoInfo = repoData;
+        this.model.setRepoInfo(this.repoInfo);
+        this.display();
       });
     });
   }
 
   display() {
-
+    // Store HTML elements in variables
+    const repoName = document.querySelector('#repo-name');
+    const repoDescription = document.querySelector('#repo-description');
+    const repoImage = document.querySelector('#repo-image')
+    
+    // Append stored repo info to each HTML element
+    repoName.append(this.model.getRepoInfo().full_name);
+    repoDescription.append(this.model.getRepoInfo().description);
+    repoImage.src = this.model.getRepoInfo().owner.avatar_url;
   }
 }
 
