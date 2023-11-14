ed21a4fd8907128e966194bb2324c3464c83ebd2
Anthony O <hackerman.ant@gmail.com>
2023-11-14 14:05:31 +0000

    add-note-button, note input box, event listener and addNoteOnClick method complete

--- resources/notes-app/index.js
+++ resources/notes-app/index.js
@@ -6,13 +6,5 @@ console.log("The notes app is running");
 // Create a new instance of NotesModel and call the addNote method to create an array of notes.
 const notesModel = new NotesModel();
 
-notesModel.addNote('Interview prep notes.');
-notesModel.addNote('Software project notes');
-notesModel.addNote('Passion project notes');
-notesModel.addNote('Workout notes');
-
 // Dependency-inject the previously created instance of NotesModel into a new NotesView instance.  
 const notesView = new NotesView(notesModel);
-notesView.displayNotes();
-
-console.log(notesModel.getNotes())
--- resources/notes-app/notesView.js
+++ resources/notes-app/notesView.js
@@ -2,17 +2,35 @@ class NotesView {
   constructor (notesModel) {
     this.notesModel = notesModel
     this.mainContainerEl = document.querySelector('#main-container');
+    this.buttonEl = document.querySelector('#add-note-button');
+    
+    this.buttonEl.addEventListener('click', () => {
+      const noteOnClick = document.querySelector('#note-input').value;
+      this.addNoteOnClick(noteOnClick);
+    });
+  }
+
+  addNoteOnClick (noteOnClick) {
+    // Add a new note to the existing array of notes
+    this.notesModel.addNote(noteOnClick);
+    
+    // Call displayNotes methods to display all notes
+    this.displayNotes();
+
+    // Empty message input element's value attribute after receiving user message input
+    document.querySelector('#note-input').value = '';
   }
 
   displayNotes () {
+    // Store array of notes in a variable
     const allNotes = this.notesModel.getNotes();
-
+    
     // A for loop that iterates through the notes array and appends a new div element to the main-container for each note element. 
     allNotes.forEach((note) => {
       let noteEl = document.createElement('div');
       noteEl.className = 'note';
-      noteEl.textContent = note;
-      this.mainContainerEl.append(noteEl);        
+      noteEl.innerHTML = note;
+      this.mainContainerEl.append(noteEl);
     });
   }
 }
