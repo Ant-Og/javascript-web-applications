336f6fa591d0d7369383fb6f18f1eab94f3acdaf
Anthony O <hackerman.ant@gmail.com>
2023-11-14 14:02:33 +0000

    new tests for event listener and emptying user input box complete

--- resources/notes-app/notesView.test.js
+++ resources/notes-app/notesView.test.js
@@ -8,43 +8,92 @@ const NotesView = require("./notesView");
 
 describe("Notes view", () => {
   
-  it("the div note element has class name 'note'", () => {
+  it("the div note element has the className 'note'", () => {
+    // Arrange
     document.body.innerHTML = fs.readFileSync("./index.html");
+    const model = new NotesModel();
+    const view = new NotesView(model);
 
-    const notesModel = new NotesModel();
-    const notesView = new NotesView(notesModel);
+    // Act
+    model.addNote("First note");
+    view.displayNotes();
+    
+    // Assert
+    expect(document.querySelector("#main-container").children[0].className).toEqual("note");
+  });
 
-    notesModel.addNote("First note");
+  it("displays no notes initially", () => {
+    // Arrange
+    document.body.innerHTML = fs.readFileSync("./index.html");
+    const model = new NotesModel();
+    const view = new NotesView(model);
 
-    notesView.displayNotes();
+    // Act
+    view.displayNotes();
 
-    expect(document.querySelector("#main-container").children[0].className).toEqual('note');
+    // Assert
+    expect(document.querySelectorAll("div.note").length).toBe(0);
   });
   
-  it("Displays one notes on the HTML page after one note is created", () => {
+  it("displays one note on the HTML page after one note is created", () => {
+    // Arrange
     document.body.innerHTML = fs.readFileSync("./index.html");
+    const model = new NotesModel();
+    const view = new NotesView(model);
 
-    const notesModel = new NotesModel();
-    const notesView = new NotesView(notesModel);
+    // Act
+    model.addNote("First note");
+    view.displayNotes();
 
-    notesModel.addNote("First note");
-
-    notesView.displayNotes();
-
-    expect(document.querySelectorAll("div.note").length).toEqual(1);
+    // Assert
+    expect(document.querySelectorAll("div.note").length).toBe(1);
   });
 
-  it("Displays two notes on the HTML page after two notes are created", () => {
+  it("displays two notes on the HTML page after two notes are created", () => {
+    // Arrange
     document.body.innerHTML = fs.readFileSync("./index.html");
+    const model = new NotesModel();
+    const view = new NotesView(model);
 
-    const notesModel = new NotesModel();
-    const notesView = new NotesView(notesModel);
+    // Act
+    model.addNote("First note");
+    model.addNote("Second note");
+    view.displayNotes();
+    
+    // Assert
+    expect(document.querySelectorAll("div.note").length).toBe(2);
+  });
 
-    notesModel.addNote("First note");
-    notesModel.addNote("Second note");
+  it("saves the users input when the add note button is clicked", () => {
+    // Arrange
+    document.body.innerHTML = fs.readFileSync("./index.html");
+    const model = new NotesModel();
+    const view = new NotesView(model);
+    const buttonEl = document.querySelector("#add-note-button");
+    const inputEl = document.querySelector("#note-input");
+    
+    // Act
+    inputEl.value = "This is an example note";
+    buttonEl.click();
 
-    notesView.displayNotes();
+    // Assert
+    expect(document.querySelectorAll("div.note").length).toBe(1);
+    expect(document.querySelectorAll("div.note")[0].textContent).toBe("This is an example note");
+  });
 
-    expect(document.querySelectorAll("div.note").length).toEqual(2);
+  it("empties the user input box after an entry is made", () => {
+    // Arrange
+    document.body.innerHTML = fs.readFileSync("./index.html");
+    const model = new NotesModel();
+    const view = new NotesView(model);
+    const buttonEl = document.querySelector("#add-note-button");
+    const inputEl = document.querySelector("#note-input");
+  
+    // Act
+    inputEl.value = "This is an example note";
+    buttonEl.click();
+    
+    // Assert
+    expect(document.querySelector("#note-input").value).toBe("");
   });
 });
