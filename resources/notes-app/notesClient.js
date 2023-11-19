478270e3c132827fd58fe2de7f526bd33a3571be
Anthony O <hackerman.ant@gmail.com>
2023-11-19 23:55:21 +0000

    partially functioning createNote method (notes tested)

--- resources/notes-app/notesClient.js
+++ resources/notes-app/notesClient.js
@@ -1,10 +1,22 @@
 class NotesClient {
   loadNotes(callback) {
-    fetch('http://localhost:3000/notes')
+    fetch("http://localhost:3000/notes")
       .then((response) => response.json())
       .then((data) => callback(data));
     }
+  
+  async createNote(data) {
+    const response = await fetch("http://localhost:3000/notes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
+    const result = await response.json();
+    console.log("Success:", result);
+  }
 }
 
 module.exports = NotesClient;
