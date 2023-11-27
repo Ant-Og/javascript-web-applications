58a852df02ff0b9787f39a6ec9ba4e9166dcce28
Anthony O <hackerman.ant@gmail.com>
2023-11-27 22:45:56 +0000

    createNote method complete

--- resources/notes-app/notesClient.js
+++ resources/notes-app/notesClient.js
@@ -1,22 +1,45 @@
 class NotesClient {
   loadNotes(callback) {
-    fetch("http://localhost:3000/notes")
+    return fetch("http://localhost:3000/notes")
       .then((response) => response.json())
       .then((data) => callback(data));
     }
   
-  async createNote(data) {
-    const response = await fetch("http://localhost:3000/notes", {
+  createNote(note) {
+    return fetch("http://localhost:3000/notes", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(data),
-    });
-
-    const result = await response.json();
-    console.log("Success:", result);
+      body: JSON.stringify({ content: note }),
+    })
+      .then((response) => response.json())
   }
+
+// Async/await version
+
+  // async loadNotes(callback) {
+  //   const response = await fetch("http://localhost:3000/notes");
+
+  //   const result = await response.json();
+  //   console.log("Success:", result);
+  //   const notes = await callback(result);
+  //   return notes
+  // }
+
+  // async createNote(note) {
+  //   const response = await fetch("http://localhost:3000/notes", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({ content: note }),
+  //   });
+    
+  //   const result = await response.json();
+  //   console.log("Success:", result);
+  //   return result
+  // }
 }
 
 module.exports = NotesClient;
